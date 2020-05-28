dd5a67d2cffa52a770413f27eac6d84d94b4b2b4
iamyanglu <499339182@qq.com>
2020-05-28 14:19:28 +0800

    '5/28'

--- src/views/demo/cloudItem.vue
+++ src/views/demo/cloudItem.vue
@@ -1,27 +1,109 @@
 <template>
+
     <div class="songItem">
-        <div class="playButton"><img src="../../assets/img/play.png" /></div>
-        <div class="songName">{{name}}</div><div class="songInfo"></div>
+        <div class="playButton" @click="starPlay"><img src="../../assets/img/play.png"  v-if="isPlay"/>
+            <img src="../../assets/img/pause.png" v-else></div>
+        <div class="songName">{{songName}}</div><div class="songInfo"></div>
     </div>
+
 </template>
 
 <script>
     export default {
         name: "cloudItem",
-        data(){
-            return {
-                imgSrc:'',
-                name:'小幸运',
-                author:'老坛酸菜',
-                songInfl:'《一首好听的歌》'
+        props:{
+            id:{
+                type:Number,
+                default: 0
+            },
+            songName:{
+                type:String,
+                default:'小歌曲'
+            },
+            playId:{
+                type:Number,
+                default: 0
 
             }
+        },
+        computed:{
+
+
+        },
+        methods:{
+            isPlayed(){
+
+               if(this.id === this.playId){
+                   this.isPlay = true
+               }
+               else {
+                   this.isPlay =false
+
+               }
+            },
+            starPlay(){
+
+                if(!this.isPlay)
+                {
+
+                    this.$emit('play',this.id)
+                }
+
+               else if(this.isPlay)
+                {
+                    this.$emit('pause',this.id)
+                }
+                this.isPlay = !this.isPlay
+
+            }
+        },
+        mounted() {
+            this.isPlayed()
+        },
+        created(){
+            console.log(this.name,'被创建了');
+        },
+        data(){
+            return {
+
+
+                isPlay:false
+            }
+        },
+        beforeDestroy() {
+            console.log(this.name,'被销毁了');
+            this.isPlay=false
         }
     }
 </script>
 
 <style scoped>
 .songItem{
-    width: 80%;
+    width: 100%;
+    border-bottom: 1px saddlebrown solid;
+    margin-top: 10px;
+    height: 49px;
+    position: relative;
+    display: flex;
+    align-items: center;
+    transition: all 0.5s;
 }
+.playButton{
+
+    margin-left: 50px;
+    margin-right: 50px;
+    width: 20px;
+    height: 20px;
+    display: inline-block;
+}
+.playButton img{
+
+        width: 20px;
+        height: 20px;
+        display: inline-block;
+    }
+.songItem div{
+    display: inline-block;
+}
+
 </style>
--- src/views/demo/cloudmusic.vue
+++ src/views/demo/cloudmusic.vue
@@ -2,10 +2,19 @@
     <div>
         <div class="musicBox">
             <div class="searchBlank">
-            <input v-model.number="keyWords" class="musicKey"><a class="search" autofocus="autofocus" @click="searchMusic">搜索</a>
+            <input v-model.number="keyWords" class="musicKey" autofocus="autofocus" /><a class="search"   @click="searchMusic">搜索</a>
             </div>
-            <div class="music_list"><cloud-item v-for="item in "></cloud-item></div>
 
+
+
+            <div class="music_list" v-if="isflesh"><cloud-item v-for="(item,key)  in    currentList" @play="play" :songName="item.name" :id="item.id" :playId="id"
+                    :key="item.id" @pause="pause"></cloud-item>
+            </div>
+
+            <div class="paPage" v-show="show && isflesh">
+                <a @click="toPage(0)">首页</a> <a @click="currentPage > 0 ? toPage(currentPage - 1) :alert('已是首页')">上一页</a>
+                <a @click="currentPage+1 < (totalSongs/6) ? toPage(currentPage + 1) :alert('已到最后一页')">下一页</a><a @click="toPage(totalSongs/6 - 1)">尾页</a><div><input v-model.Number="whPage"><div class="btnPage" @click="(totalSongs/6) >= whPage?toPage(whPage-1):alert('最多为'+totalSongs/6)">跳转</div></div>
+            </div>
 
         </div>
 
@@ -14,26 +23,72 @@
 </template>
 
 <script>
-    import {getIdList} from "../../network/musicRequest";
+    import {getIdList,getMusic} from "../../network/musicRequest";
     import cloudItem from "./cloudItem";
-    import CloudItem from "./cloudItem";
+
 
     export default {
         name: "cloudmusic",
-        components: {CloudItem},
+        components:{
+            cloudItem
+        },
+        computed:{
+            show(){
+                return this.songsList.length > 0
+            }
+
+
+        },
         data(){
             return {
-               keyWords:'',
+                whPage:'',
+                 keyWords:'',
                 totalSongs:0,
                 songsList:[],
                 totalPage:0,
+                id:0,
+                currentPage:0,
+                currentList:[],
+                Audio:null,
+                isflesh:true
 
 
             }
         },
         methods:{
-            components:{
-                cloudItem
+            alert(str){
+                window.alert(str)
+
+            },
+            pause(){
+                this.Audio.pause()
+            },
+            init(){
+                this. Audio = document.createElement('Audio')
+            },
+
+            play(id){
+                let preId = this.id;
+
+                this.id = id;
+                getMusic(this.id).then(res=>{
+
+                    this.Audio.src = res.data.data[0].url
+
+                    this.Audio.play()
+                })
+                if(preId !==0){
+                    this.isflesh = false
+                  setTimeout(()=>{
+                      this.isflesh = true
+                  },1)
+                }
+            },
+            toPage(page){
+
+               this.currentList = this.songsList.slice(page*6,page*6+6);
+                this.currentPage = page
+
             },
 
             searchMusic(){
@@ -42,8 +97,9 @@
 
                     if(res.data.code === 200){
                         let songs = res.data.result
-                        this.totalSongs = songs.songCount;
+                        this.totalSongs = songs.songs.length
                         this.songsList = songs.songs
+                          this.toPage(this.currentPage)
 
                     }
                     else{
@@ -56,7 +112,11 @@
 
         },
         created() {
+            this.init()
             this.$emit('loaded');
+        },
+        beforeDestroy() {
+            this.Audio.pause()
         }
     }
 </script>
@@ -65,8 +125,9 @@
     .musicBox{
         padding: 50px;
         width: 60%;
-        margin: 100px auto;
+        margin: 20px auto;
         border: 1px #eeeeee solid;
+        cursor: pointer;
     }
     .musicKey{
         width: 800px;
@@ -86,6 +147,10 @@
         border-shadow: 0px 1px #eeeeee;
         transition: all 0.5s;
 
+
+    }
+    .searchBlank{
+
     }
     .search:hover{
         border: 0  solid lightblue;
@@ -93,7 +158,8 @@
     }
     .music_list{
         width: 100%;
-        height: 500px;
+        height: 70%;
+        margin-bottom: 80px;
 
     }
     .searchBlank{
@@ -101,5 +167,39 @@
         display: flex;
         height: 49px;
     }
+    .paPage{
 
+
+        height: 39px;
+        display: flex;
+
+
+    }
+    .paPage a{
+        display: inline-block;
+        margin-right: 20px;
+        text-align: center;
+        line-height: 35px;
+        width: 58px;
+        height: 35px;
+        border: 1px black solid;
+    }
+    .paPage input{
+        height: 35px;
+        border: none;
+        margin-right: 10px;
+        border-bottom: 1px solid black;
+    }
+    .paPage div{
+        display: inline-block;
+    }
+    .paPage .btnPage
+    {    display: inline-block;
+        margin-right: 20px;
+        text-align: center;
+        line-height: 35px;
+        width: 58px;
+        height: 35px;
+        border: 1px black solid;
+    }
 </style>
